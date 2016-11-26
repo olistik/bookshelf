d848c166840ce4652eeb067ee6f66e77c533aea7
Maurizio De Magnis <root@olisti.co>
2016-11-26 10:58:46 +0100

    plays around with data between action, view and template

--- apps/web/views/home/index.rb
+++ apps/web/views/home/index.rb
@@ -2,8 +2,12 @@ module Web::Views::Home
   class Index
     include Web::View
 
-    # def greet
-    #   "Hello World"
-    # end
+    def greet
+      locals[:greet].upcase
+    end
+
+    def say_hi
+      greet.downcase
+    end
   end
 end
